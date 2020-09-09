466b87c44997518a748e0281f7bc42508850decb
lokman Hossen <lokman063@gmail.com>
2020-09-10 00:30:34 +0600

    prodcut relationship with category and brand completed

--- admin/views/product/create.php
+++ admin/views/product/create.php
@@ -1,5 +1,16 @@
 <?php
 include_once($_SERVER["DOCUMENT_ROOT"]."/phpcrud/bootstrap.php");
+use Bitm\Utility\Message;
+use Bitm\Utility\Utility;
+use Bitm\Category\Category;
+use Bitm\Brand\Brand;
+
+$brand = new Brand();
+$brands = $brand->all();
+
+$category = new Category();
+$categories = $category->all();
+
 ob_start();
 ?>
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
@@ -17,69 +28,99 @@ ob_start();
                         </div>
                         <div class="col-lg-6">
 
-                        
-<!--                            <div class="form-group">-->
-<!--                                <label for="brand_id">brand_id</label>-->
-<!--                                <input id="brand_id"  value="" type="text" name="brand_id" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
+
                         </div>
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="label_id">label_id</label>-->
-<!--                                <input id="label_id"  value="" type="text" name="label_id" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-                        <div class="col-lg-12">
+<!-- category name id -->
+<div class="col-lg-6">
+ <div class="form-group">
+    <label  for="inputGroupSelect01">Category Name</label>
+ 
+  <select class="custom-select" id="inputGroupSelect01">
+    <option selected>Choose...</option>
+    <?php
+    foreach ($categories as $category) {
+        ?>
+
+<option value="<?php echo $category['id'];?>"><?php echo $category['name'];?></option>
+
+        <?php
+    }
+    ?>
+   
+   
+  </select>
+  <div class="help-block with-errors"></div>
+                           </div>
+  
+</div>
+
+<!--brand name  -->
+                
+<div class="col-lg-6">
+ <div class="form-group">
+    <label  for="inputGroupSelect01">Brand Name</label>
+ 
+  <select class="custom-select" id="inputGroupSelect01">
+    <option selected>Choose...</option>
+    <?php
+    foreach ($brands as $brand) {
+        ?>
+
+<option value="<?php echo $brand['id'];?>"><?php echo $brand['title'];?></option>
+
+        <?php
+    }
+    ?>
+   
+   
+  </select>
+  <div class="help-block with-errors"></div>
+                           </div>
+  
+</div>
+                       
+           <!--product title   -->   
+           <div class="col-lg-6">
                             <div class="form-group">
                                 <label for="title">Enter Product Title</label>
                                 <input id="title"
                                        value=""
                                        type="text"
                                        name="title"
-                                       placeholder="e.g. Bashundhara Tissue"
+                                       placeholder="type here"
                                        autofocus="autofocus"
                                        class="form-control">
-                                <div class="help-block text-muted">Enter Product Title</div>
-                                <div class="help-block with-errors"></div>
+                                 <div class="help-block with-errors"></div>
                             </div>
-                        </div>
-                        <div class="col-lg-6">
+                        </div>           
+                        
+            <!-- product picture -->
+            <div class="col-lg-6">
                             <div class="form-group">
                                 <label for="picture">Picture</label>
-                                <input id="picture"  value="" type="file" name="picture" class="form-control">
+                                <input id="picture" 
+                                 value=""
+                                 type="file" 
+                                 name="picture" 
+                                  placeholder="type here"
+                                 class="form-control">
                                 <div class="help-block with-errors"></div>
                             </div>
+                         </div>
+                 <!-- MRP -->
+                     
 
                         <div class="col-lg-6">
                             <div class="form-group">
                                 <label for="mrp">MRP</label>
-                                <input id="mrp"  value="" type="text" name="mrp" class="form-control">
-                                <div class="help-block with-errors">Enter price</div>
+                                <input id="mrp" 
+                                 value="" 
+                                 placeholder="type here"
+                                type="text"
+                                 name="mrp" class="form-control">
+                             
                             </div>
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="special_price">special_price</label>-->
-<!--                                <input id="special_price"  value="" type="text" name="special_price" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="soft_delete">soft_delete</label>-->
-<!--                                <input id="soft_delete"  value="" type="text" name="soft_delete" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="is_draft">is_draft</label>-->
-<!--                                <input id="is_draft"  value="" type="text" name="is_draft" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
+
 
 
 
--- admin/views/product/edit.php
+++ admin/views/product/edit.php
@@ -76,48 +76,48 @@ ob_start();
                             ?>
 
                         </div>
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="short_description">short_description</label>-->
-<!--                                <input id="short_description"  value="" type="text" name="short_description" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="description">description</label>-->
-<!--                                <input id="description"  value="" type="text" name="description" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="total_sales">total_sales</label>-->
-<!--                                <input id="total_sales"  value="" type="text" name="total_sales" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="product_type">product_type</label>-->
-<!--                                <input id="product_type"  value="" type="text" name="product_type" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="is_new">is_new</label>-->
-<!--                                <input id="is_new"  value="" type="text" name="is_new" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="cost">cost</label>-->
-<!--                                <input id="cost"  value="" type="text" name="cost" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
+                       <div class="col-lg-6">-->
+                           <div class="form-group">
+                               <label for="short_description">short_description</label>
+                               <input id="short_description"  value="" type="text" name="short_description" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                       </div>
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <label for="description">description</label>
+                               <input id="description"  value="" type="text" name="description" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                       </div>
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <label for="total_sales">total_sales</label>
+                               <input id="total_sales"  value="" type="text" name="total_sales" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                       </div>
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <label for="product_type">product_type</label>
+                               <input id="product_type"  value="" type="text" name="product_type" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                       </div>
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <label for="is_new">is_new</label>
+                               <input id="is_new"  value="" type="text" name="is_new" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                       </div>
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <label for="cost">cost</label>
+                               <input id="cost"  value="" type="text" name="cost" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                       </div>
                         <div class="col-lg-6">
                             <div class="form-group">
                                 <label for="mrp">MRP</label>
@@ -125,27 +125,27 @@ ob_start();
                                 <div class="help-block with-errors"></div>
                             </div>
                         </div>
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="special_price">special_price</label>-->
-<!--                                <input id="special_price"  value="" type="text" name="special_price" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="soft_delete">soft_delete</label>-->
-<!--                                <input id="soft_delete"  value="" type="text" name="soft_delete" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                        <div class="col-lg-6">-->
-<!--                            <div class="form-group">-->
-<!--                                <label for="is_draft">is_draft</label>-->
-<!--                                <input id="is_draft"  value="" type="text" name="is_draft" class="form-control">-->
-<!--                                <div class="help-block with-errors"></div>-->
-<!--                            </div>-->
-<!--                        </div>-->
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <label for="special_price">special_price</label>
+                               <input id="special_price"  value="" type="text" name="special_price" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                   </div>
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <label for="soft_delete">soft_delete</label>
+                               <input id="soft_delete"  value="" type="text" name="soft_delete" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                       </div>
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <label for="is_draft">is_draft</label>
+                               <input id="is_draft"  value="" type="text" name="is_draft" class="form-control">
+                               <div class="help-block with-errors"></div>
+                           </div>
+                       </div>
                     <?php
                     $checked = '';
                     if($product['is_active']){
